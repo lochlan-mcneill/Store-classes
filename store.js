cddcd67722d2c4a7ae5d267264f33d95fc37f391
Lochlan <lochlanmcneill0@gmail.com>
2024-06-08 10:06:26 +0100

    Initial Commit
    A commit containing the first attempt at a store-like page.

---
+++ store.js
@@ -0,0 +1,49 @@
+class item {
+    //properties
+    constructor (itemName, itemPrice, itemQuantity) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemQuantity = itemQuantity;
+    }
+}
+
+
+class store {
+    //Properties
+    constructor (storeName) {
+        this.storeName = storeName;
+        this.inventory = {};
+    }
+
+    //Methods
+    add_Item(itemIn) {
+        if (itemIn instanceof item == true) {
+            
+            this.inventory[itemIn.itemName] = itemIn
+            console.log (this.inventory)
+            // console.log(`New item added to Inventory: ${this.inventory}.`)
+        }
+        else {console.log("Invalid Item.")}
+    }
+
+    removeItem(itemOut) {
+        for (i=0; i < this.inventory.itemName.length; i++) {
+            if (itemOut == item.itemName[i]) {
+
+            }
+        }
+    }
+}
+
+
+
+
+
+let Cereal = new item("JSCereal", 3, 90)
+let Milk = new item("JSMilk", 1, 20)
+
+let Tesco = new store("Tesco")
+
+Tesco.add_Item(Cereal)
+Tesco.add_Item(Milk)
+
